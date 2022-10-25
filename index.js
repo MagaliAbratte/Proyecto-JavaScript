0e2e698269e4710f65ec5c0b92490ab113b262e9
Magali Abratte <maguiabratte@gmail.com>
2022-10-25 16:41:21 -0300

    edicion de simulador interactivo

--- index.js
+++ index.js
@@ -8,8 +8,9 @@
 
 //IDENTIFICACION DEL USUARIO
 
-let nombreUsuario = prompt('- ESTUDIO CREA - Como es tu nombre?')
-alert (`Bienvenidx ${nombreUsuario}! Comencemos...`)
+alert ('-ESTUDIO CREA - Arquitectura, Diseño, Decoración & más.')
+let nombreUsuario = prompt('Como es tu nombre?')
+alert (`Bienvenidx a la seccion DECORACION ${nombreUsuario}! Encontraras muchas cosas utiles y modernas para tu hogar. Comencemos...`)
 
 //COMPRA
 
@@ -17,7 +18,7 @@ function sumaProductos (total, monto){
     return resultado = total + monto
  }
  
- let productos = parseInt(prompt('Que producto deseas comprar? 1. Mates - 2. Almohadones - 3. Canastos - 4.Plantas Medianas - 5.Plantas Grandes'))
+ let productos = parseInt(prompt('Que producto deseas comprar? (Indique el numero correspondiente a la opcion elegida, 1 - 2- 3 -4 - 5): 1. Mates - 2. Almohadones - 3. Canastos - 4.Plantas Medianas - 5.Plantas Grandes'))
  let totalCompra = 0
  let seguirComprando = true 
  let decision
@@ -34,13 +35,13 @@ function sumaProductos (total, monto){
  } else if (productos === 5){
      totalCompra = sumaProductos(totalCompra, 1800)
  }else {
-     productos = parseInt(prompt('Selecciona un producto de nuestro catalogo: 1.Mates - 2.Almohadones - 3.Canastos - 4.Plantas Medianas - 5.Plantas Grandes'))
+     productos = parseInt(prompt('Selecciona un producto disponible en nuestro catalogo (Indique el numero correspondiente a la opcion elegida, 1 - 2- 3 -4 - 5): 1.Mates - 2.Almohadones - 3.Canastos - 4.Plantas Medianas - 5.Plantas Grandes'))
      continue
  }
 
-decision = parseInt(prompt('Deseas comprar algo mas? 1.Si - 2.No'))
- if (decision === 1){
-    productos = parseInt(prompt('Que producto deseas comprar? 1.Mates - 2.Almohadones - 3.Canastos - 4.Plantas Medianas - 5.Plantas Grandes'))
+decision = prompt('Deseas comprar algo mas? Si - No')
+ if (decision === 'si'){
+    productos = parseInt(prompt('Que producto deseas comprar? (Indique el numero correspondiente a la opcion elegida, 1 - 2- 3 -4 - 5): 1.Mates - 2.Almohadones - 3.Canastos - 4.Plantas Medianas - 5.Plantas Grandes'))
  } else {
     seguirComprando = false
  }
@@ -57,16 +58,16 @@ function seisCuotas (total, seis){
     resultado = total/seis
 }
 
-let pago = parseInt(prompt('Como deseas realizar el pago? 1.Pago unico con transferencia bancaria o debito - 2. 3 cuotas sin interes con credito - 3. 6 cuotas sin interes con credito')) 
+let pago = parseInt(prompt('Como deseas realizar el pago? (Indique el numero correspondiente a la opcion elegida, 1 - 2- 3) 1.Pago unico con transferencia bancaria o debito - 2. 3 cuotas sin interes con credito - 3. 6 cuotas sin interes con credito')) 
 
 if (pago === 1){
-    alert (`Pago unico de ${totalCompra}. Para continuar, realiza la transferencia a estudio.crea o ingresa los datos de la tarjeta en el formulario correspondiente.`)
+    alert (`Pago unico de $${totalCompra}. Para continuar, realiza la transferencia a estudio.crea o ingresa los datos de la tarjeta en el formulario correspondiente.`)
 } else if (pago === 2){
     tresCuotas (totalCompra, 3)
-    alert (`El pago de tu compra se realizara en tres cuotas de ${resultado} cada una. Para continuar, ingresa los datos de la tarjeta en el formulario correspondiente.`)
+    alert (`El pago de tu compra se realizara en tres cuotas de $${resultado} cada una. Para continuar, ingresa los datos de la tarjeta en el formulario correspondiente.`)
 } else if (pago === 3){
     seisCuotas (totalCompra, 6)
-    alert (`El pago de tu compra se realizara en seis cuotas de ${resultado} cada una. Para continuar, ingresa los datos de la tarjeta en el formulario correspondiente.`)
+    alert (`El pago de tu compra se realizara en seis cuotas de $${resultado} cada una. Para continuar, ingresa los datos de la tarjeta en el formulario correspondiente.`)
 }
 
 alert (`${nombreUsuario}, agradecemos tu compra y esperamos volver a verte pronto :) - ESTUDIO CREA -`)
