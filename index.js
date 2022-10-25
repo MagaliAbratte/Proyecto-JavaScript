67182a26c8eab458868cf3f18a5f22b3cbbbcc1f
Magali Abratte <maguiabratte@gmail.com>
2022-10-25 17:02:08 -0300

    edicion de simulador interactivo

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sumaProductos (total, monto){
     return resultado = total + monto
  }
  
- let productos = parseInt(prompt('Que producto deseas comprar? (Indique el número correspondiente a la opción elegida, 1 - 2- 3 -4 - 5): 1. Mates - 2. Almohadones - 3. Canastos - 4.Plantas Medianas - 5.Plantas Grandes'))
+ let productos = parseInt(prompt('Que producto deseas comprar? (Indique el número correspondiente a la opción elegida, 1 - 2 - 3 - 4 - 5): 1. Mates - 2. Almohadones - 3. Canastos - 4.Plantas Medianas - 5.Plantas Grandes'))
  let totalCompra = 0
  let seguirComprando = true 
  let decision
@@ -35,13 +35,13 @@ function sumaProductos (total, monto){
  } else if (productos === 5){
      totalCompra = sumaProductos(totalCompra, 1800)
  }else {
-     productos = parseInt(prompt('Selecciona un producto disponible en nuestro catalogo (Indica el número correspondiente a la opción elegida, 1 - 2- 3 -4 - 5): 1.Mates - 2.Almohadones - 3.Canastos - 4.Plantas Medianas - 5.Plantas Grandes'))
+     productos = parseInt(prompt('Selecciona un producto disponible en nuestro catalogo (Indica el número correspondiente a la opción elegida, 1 - 2 - 3 - 4 - 5): 1.Mates - 2.Almohadones - 3.Canastos - 4.Plantas Medianas - 5.Plantas Grandes'))
      continue
  }
 
 decision = prompt('Deseas comprar algo mas? Si - No')
  if (decision === 'si'){
-    productos = parseInt(prompt('Que producto deseas comprar? (Indica el número correspondiente a la opción elegida, 1 - 2- 3 -4 - 5): 1.Mates - 2.Almohadones - 3.Canastos - 4.Plantas Medianas - 5.Plantas Grandes'))
+    productos = parseInt(prompt('Que producto deseas comprar? (Indica el número correspondiente a la opción elegida, 1 - 2 - 3 - 4 - 5): 1.Mates - 2.Almohadones - 3.Canastos - 4.Plantas Medianas - 5.Plantas Grandes'))
  } else {
     seguirComprando = false
  }
@@ -58,7 +58,7 @@ function seisCuotas (total, seis){
     resultado = total/seis
 }
 
-let pago = parseInt(prompt('Como deseas realizar el pago? (Indica el número correspondiente a la opción elegida, 1 - 2- 3) 1.Pago único con transferencia bancaria o débito - 2. 3 cuotas sin interes con crédito - 3. 6 cuotas sin interes con crédito')) 
+let pago = parseInt(prompt('Como deseas realizar el pago? (Indica el número correspondiente a la opción elegida, 1 - 2 - 3) 1.Pago único con transferencia bancaria o débito - 2. 3 cuotas sin interes con crédito - 3. 6 cuotas sin interes con crédito')) 
 
 if (pago === 1){
     alert (`Pago único de $${totalCompra}. Para continuar, realiza la transferencia a estudio.crea o ingresa los datos de la tarjeta en el formulario correspondiente.`)
@@ -68,6 +68,6 @@ if (pago === 1){
 } else if (pago === 3){
     seisCuotas (totalCompra, 6)
     alert (`El pago de tu compra se realizara en seis cuotas de $${resultado} cada una. Para continuar, ingresa los datos de la tarjeta en el formulario correspondiente.`)
-}
+} 
 
 alert (`${nombreUsuario}, agradecemos tu compra y esperamos volver a verte pronto :) - ESTUDIO CREA -`)
